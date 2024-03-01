44c27714d06e52dd7100641ac8c4a9ce2872c66f
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-01 13:21:22 +0000

    Update Team.java

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -6,5 +6,18 @@ import java.util.ArrayList;
 public class Team {
   string name ;
   string description;
+  static int nextTeamId = 0;
+  int id;
+  ArrayList<Rider> riders;
 
-  
+  public Team(String name, String description) {
+    this.name = name;
+    this.description = description;
+    this.id = nextTeamId;
+    nextTeamId++;
+    this.riders = new ArrayList<Rider>();
+  }
+
+  public string getName() {
+    return name;
+  }
