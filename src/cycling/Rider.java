ba0106290a143d00f4094d65d76abd47285e36ca
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-02 16:52:35 +0000

    Update Rider.java

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -1,1 +1,26 @@
+package cycling;
 
+public class Rider {
+    int teamID;
+    String name;
+    int yearOfBirth;
+    int riderId;
+    static int nextRiderId = 0;
+
+    public Rider(int teamID, String name, int yearOfBirth) {
+        this.teamID = teamID;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.riderId = nextRiderId;
+        nextRiderId++;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public int getId() {
+        return riderId;
+    }
+
+}
