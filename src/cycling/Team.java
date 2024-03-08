0402df49817e06a6413555552735516a3c95ca3a
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-08 17:44:59 +0000

    Update Team.java

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -4,32 +4,33 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Team {
-  string name ;
-  string description;
-  static int nextTeamId = 0;
-  int id;
-  ArrayList<Rider> riders;
+    String name;
+    String description;
+    static int nextTeamId = 0;
+    int id;
+    ArrayList<Rider> riders;
 
-  public Team(String name, String description) {
-    this.name = name;
-    this.description = description;
-    this.id = nextTeamId;
-    nextTeamId++;
-    this.riders = new ArrayList<Rider>();
-  }
+    public Team(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.id = nextTeamId;
+        nextTeamId++;
+        this.riders = new ArrayList<Rider>();
+    }
 
-  public string getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-   public void addRider(int teamId, String name, int yearOB){
-        riders.add(new Rider(teamId,name,yearOB));
-  }
-  public void removeRider(Rider astolfo){
+    public void addRider(int teamId, String name, int yearOB) {
+        riders.add(new Rider(teamId, name, yearOB));
+    }
+
+    public void removeRider(Rider astolfo) {
         riders.remove(astolfo);
-  }
+    }
 }
