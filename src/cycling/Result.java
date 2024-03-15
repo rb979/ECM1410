f1b18fb5a5fa282d1bfe8dbee3a07bd5fe300a1b
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-15 18:23:50 +0000

    Create Result.java

---
+++ src/cycling/Result.java
@@ -0,0 +1,106 @@
+package cycling;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Result keeps track of results within a certain stage
+ * @author Ryan Butler
+ */
+public class Result implements java.io.Serializable {
+    private int raceId;
+    private int stageId;
+    private int riderId;
+    private int resultId;
+
+    private LocalTime resultElapsedTime;
+    private int points = 0;
+    private int mountainPoints = 0;
+
+    private static ArrayList<Integer> resultIds = new ArrayList<Integer>();
+
+    private ArrayList<LocalTime> checkpoints = new ArrayList<LocalTime>();
+
+    public Result(int raceId, int stageId, int riderId, LocalTime... checkpoints) {
+        this.raceId = raceId;
+        this.stageId = stageId;
+        this.riderId = riderId;
+
+        for(LocalTime check : checkpoints) {
+            this.checkpoints.add(check);
+        }
+
+        if(resultIds.size() == 0) {
+            resultId = 0;
+            resultIds.add(resultId);
+        } else {
+            resultId = resultIds.get(resultIds.size() - 1) + 1;
+            resultIds.add(resultId);
+        }
+    }
+
+    public LocalTime getResultElapsedTime() {
+        return resultElapsedTime;
+    }
+
+    public void setResultElapsedTime(LocalTime resultElapsedTime) {
+        this.resultElapsedTime = resultElapsedTime;
+    }
+
+    public int getRaceId() {
+        return raceId;
+    }
+
+    public int getResultId() {
+        return resultId;
+    }
+
+    public int getRiderId() {
+        return riderId;
+    }
+
+    public ArrayList<LocalTime> getCheckpoints() {
+        return checkpoints;
+    }
+
+    public int getStageId() {
+        return stageId;
+    }
+
+    public LocalTime getFinishTime() {
+        return checkpoints.get(checkpoints.size() - 1);
+    }
+
+    public void setFinishTime(LocalTime finishTime) {
+        checkpoints.remove(checkpoints.size() - 1);
+        checkpoints.add(finishTime);
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+    public int getMountainPoints() {
+        return mountainPoints;
+    }
+
+    public void addMountainPoints(int mountainPoints) {
+        this.mountainPoints += mountainPoints;
+    }
+
+
+    public static LocalTime getElapsedTime(LocalTime startTime, LocalTime finishTime) {
+        int hours = (int) ChronoUnit.HOURS.between(startTime, finishTime);
+        int minutes = (int) ChronoUnit.MINUTES.between(startTime, finishTime) % 60;
+        int seconds = (int) ChronoUnit.SECONDS.between(startTime, finishTime) % 60;
+        return LocalTime.of(hours, minutes, seconds);
+    }
+
+}
