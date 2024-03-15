62eabd317e5deec20640af2e9f71f5d4972b0df5
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-15 18:33:53 +0000

    Update Rider.java

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -1,5 +1,8 @@
 package cycling;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A class used to create an instance of a rider.
  * These riders will belong to an instance of a team class.
@@ -7,13 +10,13 @@ package cycling;
  * @author Ryan Butler
  * 
  */
-
 public class Rider {
-    int teamID;
-    String name;
-    int yearOfBirth;
-    int riderId;
-    static int nextRiderId = 0;
+    private int teamID;
+    private String name;
+    private int yearOfBirth;
+    private int riderId;
+    private static int nextRiderId = 0;
+    private Map<Integer, LocalTime[]> resultsByStage; // Maps stage IDs to arrays of checkpoint times
 
     public Rider(int teamID, String name, int yearOfBirth) {
         this.teamID = teamID;
@@ -21,6 +24,7 @@ public class Rider {
         this.yearOfBirth = yearOfBirth;
         this.riderId = nextRiderId;
         nextRiderId++;
+        this.resultsByStage = new HashMap<>();
     }
 
     public int getTeamID() {
@@ -31,4 +35,18 @@ public class Rider {
         return riderId;
     }
 
+    // Method to register results for a specific stage
+    public void registerResultsForStage(int stageId, LocalTime[] checkpoints) {
+        resultsByStage.put(stageId, checkpoints);
+    }
+
+    // Method to check if results are already registered for a stage
+    public boolean hasResultsForStage(int stageId) {
+        return resultsByStage.containsKey(stageId);
+    }
+
+    // Getter for all results
+    public Map<Integer, LocalTime[]> getAllResults() {
+        return resultsByStage;
+    }
 }
