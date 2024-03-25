becf766bbc1b29a5da1eded473cb3db6b4c0b9df
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-25 20:02:46 +0000

    Update BadCyclingPortalImpl.java

--- src/cycling/BadCyclingPortalImpl.java
+++ src/cycling/BadCyclingPortalImpl.java
@@ -135,63 +135,22 @@ public class BadCyclingPortalImpl implements CyclingPortal {
 
 	@Override
 	public int createTeam(String name, String description) throws IllegalNameException, InvalidNameException {
-	    InvalidNameException.checkName(name);
-	    for (Team t : teams) {
-	        if (name.equals(t.getName())) {
-	            throw new IllegalNameException("Name already used");
-	        }
-	    }
-	    Team newTeam = new Team(name, description);
-	    teams.add(newTeam);
-	    return newTeam.getId(); 
+	    return Team.createTeam(name, description);
 	}
 	
 	@Override
 	public void removeTeam(int teamId) throws IDNotRecognisedException {
-	    Iterator<Team> iterator = teams.iterator();
-	    boolean check = false;
-	    while (iterator.hasNext()) {
-	        Team t = iterator.next();
-	        if (teamId == t.getId()) {
-	            iterator.remove();
-	            check = true;
-	            break;
-	        }
-	    }
-	    if (!check) {
-	        throw new IDNotRecognisedException("ID Not Recognised");
-	    }
-	}
+        	Team.removeTeam(teamId);
+    	}
 	
 	@Override
 	public int[] getTeams() {
-	    int[] teamIds = new int[teams.size()];
-	    int i = 0;
-	    for (Team t : teams) {
-	        teamIds[i] = t.getId();
-	        i++;
-	    }
-	    return teamIds;
+	    return Team.getTeams();
 	}
 	
 	@Override
 	public int[] getTeamRiders(int teamId) throws IDNotRecognisedException {
-	    for (Team t : teams) {
-	        if (t.getId() == teamId) {
-	            List<Rider> riders = t.getRiders();
-	            if (riders.size() == 0) {
-	                return null;
-	            }
-	            int[] riderIds = new int[riders.size()];
-	            int i = 0;
-	            for (Rider r : riders) {
-	                riderIds[i] = r.getId();
-	                i++;
-	            }
-	            return riderIds;
-	        }
-	    }
-	    throw new IDNotRecognisedException("ID Not Recognised");
+	    return Team.getTeamRiders(teamId);
 	}
 	
 	@Override
