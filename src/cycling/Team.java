34c8194cf41244851af83c8681a0318427afaf4e
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-25 20:02:02 +0000

    Update Team.java

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -49,4 +49,60 @@ public class Team {
     public List<Rider> getRiders() {
         return riders;
     }
+    
+    public static int createTeam(String name, String description) throws IllegalNameException, InvalidNameException {
+        for (Team t : teams) {
+            if (name.equals(t.getName())) {
+                throw new IllegalNameException("Name already used");
+            }
+        }
+        Team newTeam = new Team(name, description);
+        teams.add(newTeam);
+        return newTeam.getId();
+    }
+    
+    public static void removeTeam(int teamId) throws IDNotRecognisedException {
+	    Iterator<Team> iterator = teams.iterator();
+	    boolean check = false;
+	    while (iterator.hasNext()) {
+	        Team t = iterator.next();
+	        if (teamId == t.getId()) {
+	            iterator.remove();
+	            check = true;
+	            break;
+	        }
+	    }
+	    if (!check) {
+	        throw new IDNotRecognisedException("ID Not Recognised");
+	    }
+	}
+    
+    public static int[] getTeams() {
+	    int[] teamIds = new int[teams.size()];
+	    int i = 0;
+	    for (Team t : teams) {
+	        teamIds[i] = t.getId();
+	        i++;
+	    }
+	    return teamIds;
+	}
+   
+    public static int[] getTeamRiders(int teamId) throws IDNotRecognisedException {
+        for (Team t : teams) {
+            if (t.getId() == teamId) {
+                List<Rider> riders = t.getRiders();
+                if (riders.size() == 0) {
+                    return null;
+                }
+                int[] riderIds = new int[riders.size()];
+                int i = 0;
+                for (Rider r : riders) {
+                    riderIds[i] = r.getId();
+                    i++;
+                }
+                return riderIds;
+            }
+        }
+        throw new IDNotRecognisedException("ID Not Recognised");
+    }
 }
