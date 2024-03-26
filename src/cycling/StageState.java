63251852a705c19c3fdb14b9934205b7193fb749
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-26 12:10:40 +0000

    Update StageState.java

--- src/cycling/StageState.java
+++ src/cycling/StageState.java
@@ -1,5 +1,11 @@
 package cycling;
-
+/**
+ * 
+ * This Enum used to represent the different states a stage can be in
+ * 
+ * @author Ryan Butler 
+ * 
+ */
 public enum StageState {
     UNDER_PREPARATION,
     WAITING_FOR_RESULTS,
