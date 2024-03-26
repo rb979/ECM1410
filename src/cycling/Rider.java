ca9740ba16e1fe31f1f29d4055779275f5fcd4c4
rb979 <161337332+rb979@users.noreply.github.com>
2024-03-26 12:19:47 +0000

    Update Rider.java

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static cycling.Team.teams;
+
 /**
  * A class used to create an instance of a rider.
  * These riders will belong to an instance of a team class.
@@ -31,12 +33,50 @@ public class Rider {
         riders.add(this);
     }
 
+    public static int getNextRiderId() {
+        return nextRiderId;
+    }
+    
     public int getTeamID() {
         return teamID;
     }
 
     public int getId() {
         return riderId;
+    }
+
+    public static int createRider(int teamID, String name, int yearOfBirth) throws IDNotRecognisedException, IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name can't be empty");
+        }
+        if (yearOfBirth < 1900) {
+            throw new IllegalArgumentException("Invalid Year of Birth");
+        }
+        try {
+            for (Team t : teams) {
+                if (teamID == t.getId()) {
+                    t.addRider(name, yearOfBirth);
+                    return Rider.getNextRiderId();
+                }
+            }
+            throw new IDNotRecognisedException("Team ID Not Recognised");
+        } catch (IDNotRecognisedException e) {
+            throw e;
+        }
+    }
+
+    public static void removeRider(int riderId) throws IDNotRecognisedException {
+        try{
+            for(Team t:teams){
+                for(Rider r: t.getRiders()){
+                    if(r.getId()==riderId){
+                        t.removeRider(r);
+                    }
+                }
+            }throw new IDNotRecognisedException("ID Not Recognised");
+        }catch(IDNotRecognisedException e){
+            System.out.println(e);
+        }
     }
 
     public void setResultsForStage(int stageId, LocalTime[] checkpoints) {
